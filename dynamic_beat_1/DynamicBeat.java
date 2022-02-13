1dd92a11a0a5da1f559a0bc7d70a7817aa5373d7
brown <ohbass95@gmail.com>
2022-02-13 10:34:44 +0900

    D01-1

--- dynamic_beat_1/DynamicBeat.java
+++ dynamic_beat_1/DynamicBeat.java
@@ -1,8 +1,17 @@
 package dynamic_beat_1;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class DynamicBeat extends JFrame {
+	
+	private Image screenImage;
+	private Graphics screenGraphic;
+	
+	private Image introBackground;
 	
 	public DynamicBeat() {
 		setTitle("Dynamic Beat");
@@ -11,6 +20,21 @@ public class DynamicBeat extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		introBackground 
+		= new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
 	}
-
+	
+	public void paint(Graphics g) {
+	 screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+	 screenGraphic = screenImage.getGraphics();
+	 screenDraw(screenGraphic);
+	 g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();
+	}
+	
 }
